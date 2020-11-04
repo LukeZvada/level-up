03247ebc55206ee6777ee7352402c2fad3371762
LukeZvada <zvadamusic@gmail.com>
2020-11-04 11:14:03 -0600

    through ch 7

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,22 +1,30 @@
-"""levelup URL Configuration
+# pylint: disable=missing-module-docstring
+from django.conf.urls import include
+from django.urls import path
+from rest_framework import routers
+from levelupapi.views import register_user, login_user
+from levelupapi.views import Games, GameTypes, Events, Profile
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#####################################
+##                                 ##
+##           Your new              ##
+##      request_handler.py         ##
+##                                 ##
+#####################################
+
+
+# pylint: disable=invalid-name
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'games', Games, 'game')
+router.register(r'gametypes', GameTypes, 'gametype')
+router.register(r'events', Events, 'event')
+router.register(r'profile', Profile, 'profile')
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # http://localhost:8000/register
+    path('', include(router.urls)),
     path('register', register_user),
-    # http://localhost:8000/login
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
